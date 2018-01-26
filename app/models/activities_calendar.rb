5b3135743fa1a536bb16e01ed9cbad078a655496
krzowski <krz.oldakowski@gmail.com>
2018-01-26 13:21:59 +0100

    create ActivitiesCalendar and Activity models and add associations

---
+++ app/models/activities_calendar.rb
@@ -0,0 +1,3 @@
+class ActivitiesCalendar < ActiveRecord::Base
+  belongs_to :user
+end
---
+++ db/migrate/20180126115741_create_activities_calendars.rb
@@ -0,0 +1,9 @@
+class CreateActivitiesCalendars < ActiveRecord::Migration
+  def change
+    create_table :activities_calendars do |t|
+      t.references :user, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end

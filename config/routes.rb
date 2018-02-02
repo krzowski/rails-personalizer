6d16031bcd8a599eebe7373a16c69143b6a7af5e
krzowski <krz.oldakowski@gmail.com>
2018-02-02 14:41:59 +0100

    rails add users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
   resource :food, only: [:create, :destroy]
   resource :exercise, only: [:create, :destroy]
 
+  patch 'goals', to: 'users#change_goals'
+
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

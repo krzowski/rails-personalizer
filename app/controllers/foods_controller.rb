7e24bc3cdad75ab90a3feabbc72f938473ec87c8
krzowski <krz.oldakowski@gmail.com>
2018-02-02 15:35:30 +0100

    add food_form to calendar and corresponding create action in foods_controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,7 +1,24 @@
 class FoodsController < ApplicationController
   def create
+    activities_list = current_user.daily_activities.find_by(date: params[:date])
+    unless activities_list
+      activities_list = current_user.daily_activities.create(date: params[:date])
+    end
+
+    food = activities_list.foods.new(food_params)
+    if food.save
+      flash[:success] = "food was added to the list"
+    else
+      flash[:danger] = "food didnt save"
+    end
+    redirect_to calendar_path
   end
 
   def destroy
   end
+
+  private
+    def food_params
+      params.require(:food).permit(:name, :calories)
+    end
 end

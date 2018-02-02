04646d202aaa5127f6d6572d8f1d546de7f6c3ed
krzowski <krz.oldakowski@gmail.com>
2018-02-02 11:54:47 +0100

    style the calendar and display food/exercise calories counters

--- app/models/daily_activity.rb
+++ app/models/daily_activity.rb
@@ -2,4 +2,12 @@ class DailyActivity < ActiveRecord::Base
   belongs_to :user
   has_many :exercises
   has_many :foods
+
+  def exercise_calories
+    self.exercises.sum(:calories)
+  end
+
+  def food_calories
+    self.foods.sum(:calories)
+  end
 end

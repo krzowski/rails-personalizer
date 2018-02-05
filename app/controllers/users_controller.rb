fc50241ea0ef970bc093e2fe231793cd6de517b7
krzowski <krz.oldakowski@gmail.com>
2018-02-05 10:49:55 +0100

    add and style goals form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def change_goals
     if true
-      current_user.update_attributes(food_goal), exercise_goal
+      current_user.update_attributes(food_goal, exercise_goal)
     end
   end
 end

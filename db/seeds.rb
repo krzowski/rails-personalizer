90567acbbe67bba28a14fc66b5588dea16645524
Krzysztof Oldakowski <krz.oldakowski@gmail.com>
2018-04-12 16:45:18 +0200

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@ user = User.create(email: "testaccount@example.com", password: "qwerty")
 date = Date.today - 10.days
 40.times do
   activity = user.daily_activities.create(date: date)
-  5.times { |i| activity.foods.create("food-#{i+1}", calories: (rand(200) + 100)) }
-  5.times { |i| activity.exercises.create("exercise-#{i+1}", calories: (rand(200) + 100)) }
+  5.times { |i| activity.foods.create(name: "food-#{i+1}", calories: (rand(200) + 100)) }
+  5.times { |i| activity.exercises.create(name: "exercise-#{i+1}", calories: (rand(200) + 100)) }
 
   date = date + 1.day
 end
